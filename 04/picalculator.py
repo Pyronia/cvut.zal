7d8ab1eafcf79718d1a79a3f01d75f44799ab921
Pyronia <michal.hubalek@gmail.com>
2017-10-23 21:10:33 +0200

    leibnizPi + nilakanthaPi

--- 04/picalculator.py
+++ 04/picalculator.py
@@ -3,21 +3,59 @@ def newtonPi(init):
 
 
 def leibnizPi(iterations):
-    pass
+    dividend = 4
+    divisor = 1
+    iteration = 0
+    pi = 0
+    odd = True
+
+    while iteration < iterations:
+        if odd:
+            pi += dividend / divisor
+        else:
+            pi -= dividend / divisor
+
+        divisor += 2
+        iteration =+ 1
+        odd = not odd
+
+    print('Iterations = ' + str(iterations))
+    return pi
 
 
 def nilakanthaPi(iterations):
-    pass
+    dividend = 4
+    divisor = 2
+    iteration = 0
+    pi = 3
+    odd = True
 
+    while iteration < iterations:
+        if odd:
+            pi += dividend / divisor * (divisor + 1) * (divisor + 2)
+        else:
+            pi -= dividend / divisor * (divisor + 1) * (divisor + 2)
 
+        divisor += 2
+        iteration += 1
+        odd = not odd
+
+    print('Iterations = ' + str(iterations))
+    return pi
 
 def testNewtonPi(init):
     pass
 
 
 def testLeibnizPi(iterations):
-    pass
+    print("Expected iterations = " + str(iterations) +
+          "\nPi = " + str(leibnizPi(iterations)))
 
 
 def testNilakanthaPi(iterations):
-    pass
+    print("Expected iterations = " + str(iterations) +
+          "\nPi = " + str(testNilakanthaPi(iterations)))
+
+
+testLeibnizPi(5000000)
+testNilakanthaPi(5000000)
