ee2dbacb528fc82d7259ff0d5bcda51322eb8a14
bh063 <michal.hubalek.ext@deutsche-boerse.com>
2017-10-23 14:25:53 +0200

    declarations

---
+++ 04/picalculator.py
@@ -0,0 +1,23 @@
+def newtonPi(init):
+    pass
+
+
+def leibnizPi(iterations):
+    pass
+
+
+def nilakanthaPi(iterations):
+    pass
+
+
+
+def testNewtonPi(init):
+    pass
+
+
+def testLeibnizPi(iterations):
+    pass
+
+
+def testNilakanthaPi(iterations):
+    pass
