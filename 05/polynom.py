c5342176794a66a63a6893db7c4f234720644743
Michal Hubálek <hubalmic@fel.cvut.cz>
2017-11-02 22:31:16 +0100

    polyEval

--- 05/polynom.py
+++ 05/polynom.py
@@ -1,6 +1,11 @@
 def polyEval(poly, x):
-    pass
+    result = 0
+    i = len(poly)
 
+    while i > 0:
+        result = result * x + poly[i]
+
+    return result
 
 def polySum(poly1, poly2):
     pass
@@ -8,3 +13,19 @@ def polySum(poly1, poly2):
 
 def polyMultiply(poly1, poly2):
     pass
+
+
+def runTests():
+    result = polyEval([1, 2.5, 3.5, 0, 5.4], 0)
+    expected = 1
+    if result != expected:
+        print('\n*/*/*/*/* UNEXPECTED RESULT */*/*/*/*')
+        print('polyEval([1, 2.5, 3.5, 0, 5.4], 0), expected: ' + str(expected) + ' actual: ' + str(result))
+        print('*/*/*/*/* UNEXPECTED RESULT */*/*/*/*\n')
+
+    result = polyEval([1, 2.5, 3.5, 0, 5.4], 2)
+    expected = 106.4
+    if result != expected:
+        print('\n*/*/*/*/* UNEXPECTED RESULT */*/*/*/*')
+        print('polyEval([1, 2.5, 3.5, 0, 5.4], 2), expected: ' + str(expected) + ' actual: ' + str(result))
+        print('*/*/*/*/* UNEXPECTED RESULT */*/*/*/*\n')
