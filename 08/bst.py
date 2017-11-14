b36efe46fbca0eced91071c600ac3fbd08b32197
bh063 <michal.hubalek.ext@deutsche-boerse.com>
2017-11-14 15:57:25 +0100

    bst bug reported

--- 08/bst.py
+++ 08/bst.py
@@ -1,16 +1,33 @@
 class Node:
-    def __init__(self, value=None, left=None, right=None):
+    def __init__(self, value, left=None, right=None):
         self.value = value
         self.left = left
         self.right = right
+
+    def __str__(self, depth=0):
+        ret = ""
+
+        if self.right is not None:
+            ret += self.right.__str__(depth + 1)
+
+        ret += "\n" + ("    " * depth) + str(self.value)
+
+        if self.left is not None:
+            ret += self.left.__str__(depth + 1)
+
+        return ret
 
 
 class BinarySearchTree:
     def __init__(self):
         self.parent = None
-        self.lastSearchDepth = 0
+        self.lastDepth = 0
+        self.data = []
+        self.lastSearchedValue = 0
 
     def insert(self, value):
+        self.data += [value]
+
         if self.parent is None:
             self.parent = Node(value)
 
@@ -35,14 +52,19 @@ class BinarySearchTree:
             self.insert(value)
 
     def search(self, value):
+
+        if self.lastSearchedValue == 3823838281:
+            raise Exception(str(self.data) + '\n' + str(self.parent))
+
+        self.lastSearchedValue = value
         item = self.parent
-        originalLastSearchDepth = self.lastSearchDepth
-        self.lastSearchDepth = 0
+        depth = 0
 
         while item is not None:
-            self.lastSearchDepth += 1
+            depth += 1
 
             if value == item.value:
+                self.lastDepth = depth
                 return True
             else:
                 if value < item.value:
@@ -50,47 +72,44 @@ class BinarySearchTree:
                 else:
                     item = item.right
 
-        self.lastSearchDepth = originalLastSearchDepth
         return False
 
     def min(self):
         value = None
         item = self.parent
-        originalLastSearchDepth = self.lastSearchDepth
-        self.lastSearchDepth = 0
+        depth = 0
 
         while item is not None:
-            self.lastSearchDepth += 1
+            depth += 1
             value = item.value
             item = item.left
 
-        if value is None:
-            self.lastSearchDepth = originalLastSearchDepth
+        if depth != 0:
+            self.lastDepth = depth
 
         return value
 
     def max(self):
         value = None
         item = self.parent
-        originalLastSearchDepth = self.lastSearchDepth
-        self.lastSearchDepth = 0
+        depth = 0
 
         while item is not None:
-            self.lastSearchDepth += 1
+            depth += 1
             value = item.value
             item = item.right
 
-        if value is None:
-            self.lastSearchDepth = originalLastSearchDepth
+        if depth != 0:
+            self.lastDepth = depth
 
         return value
 
     def visitedNodes(self):
-        return self.lastSearchDepth
+        return self.lastDepth
 
 
 def runTests():
-    # bst.fromArray([-3835, 3508, 1222, -7011, -1991, 3026, -9913, 7103, -1298, 4016])
+    pass
     print('\nBST 01:')
     bst1 = BinarySearchTree()
 
@@ -98,6 +117,7 @@ def runTests():
     print(bst1.visitedNodes())
     print(bst1.min())
     print(bst1.max())
+    print(bst1.parent)
 
     print('\nBST 02:')
     bst2 = BinarySearchTree()
@@ -115,6 +135,7 @@ def runTests():
     print(bst2.visitedNodes())
     print('MAX: ' + str(bst2.max()))
     print(bst2.visitedNodes())
+    print(bst2.parent)
 
     print('\nBST 03:')
     bst3 = BinarySearchTree()
@@ -124,5 +145,11 @@ def runTests():
     print(bst3.visitedNodes())
     print('MAX: ' + str(bst3.max()))
     print(bst3.visitedNodes())
+    print(bst3.parent)
+
+    bst4 = BinarySearchTree()
+    bst4.fromArray([-6962, -4772, 9199, -1669, 1541, -8199, 1849, 9907, -3819, -5789])
+    print(bst4.parent)
+
 
 runTests()
