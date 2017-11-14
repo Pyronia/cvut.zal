251e30ada56fb5dead58ad3b4e18dfa8fd8251f0
bh063 <michal.hubalek.ext@deutsche-boerse.com>
2017-11-14 10:42:14 +0100

    dijkstra tests commit

--- 10/dijkstra.py
+++ 10/dijkstra.py
@@ -26,3 +26,37 @@ class Dijkstra:
 
     def getVertexes(self):
         pass
+
+
+#
+# def runTests():
+#     # New Dijkstra created
+#     dijkstra = Dijkstra()
+#     # Graph created
+#     dijkstra.createGraph(vertexes, edges)
+#     # Getting all vertexes
+#     dijkstraVertexes = dijkstra.getVertexes()
+#     # Computing min distance for each vertex in graph
+#     for vertexToCompute in dijkstraVertexes:
+#         dijkstra.computePath(vertexToCompute.id)
+#         print('Printing min distance from vertex:' + str(vertexToCompute.name))
+#         # Print minDitance from current vertex to each other
+#         for vertex in dijkstraVertexes:
+#             print('Min distance to:' + str(vertex.name) + ' is: ' + str(vertex.minDistance))
+#         # Reset Dijkstra between counting
+#         dijkstra.resetDijkstra()
+#     # Distance with path
+#     for vertexToCompute in dijkstraVertexes:
+#         dijkstra.computePath(vertexToCompute.id)
+#         print('Printing min distance from vertex:' + str(vertexToCompute.name))
+#         # Print minDitance and path from current vertex to each other
+#         for vertex in dijkstraVertexes:
+#             print('Min distance to:' + str(vertex.name) + ' is: ' + str(vertex.minDistance))
+#             print('Path is:', end=" ")
+#             # Get shortest path to target vertex
+#             path = dijkstra.getShortestPathTo(vertex.id)
+#             for vertexInPath in path:
+#                 print(str(vertexInPath.name), end=" ")
+#                 print()
+#             # Reset Dijkstra between counting
+#             dijkstra.resetDijkstra()
