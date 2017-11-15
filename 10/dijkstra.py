c606e56f9f1f6cb1dc0be46b8dd8fbdd1ebd8aa8
bh063 <michal.hubalek.ext@deutsche-boerse.com>
2017-11-15 11:07:14 +0100

    Dijkstrause init methods + commented "test" from teacher

--- 10/dijkstra.py
+++ 10/dijkstra.py
@@ -1,19 +1,33 @@
 class Vertex:
     def __init__(self, id, name):
-        pass
+        self.id = id
+        self.name = name
+        self.minDistance = -1
+        self.previousVertex = None
+        self.edges = []
+
+    def addEdge(self, edge):
+        self.edges.append(edge)
 
 
 class Edge:
     def __init__(self, source, target, weight):
-        pass
+        self.source = source
+        self.target = target
+        self.weight = weight
 
 
 class Dijkstra:
-    def __init__(self):
-        pass
+    def __init__(self, vertexes):
+        self.vertexes = vertexes
+
+    def resetDijkstra(self):
+        for vertex in self.vertexes:
+            vertex.minDistance = float('Inf')
+            vertex.previousVertex = None
 
     def computePath(self, sourceId):
-        pass
+        self.resetDijkstra()
 
     def getShortestPathTo(self, targetId):
         pass
@@ -21,42 +35,12 @@ class Dijkstra:
     def createGraph(self, vertexes, edgesToVertexes):
         pass
 
-    def resetDijkstra(self):
-        pass
-
     def getVertexes(self):
-        pass
+        return self.vertexes
 
 
-#
-# def runTests():
-#     # New Dijkstra created
-#     dijkstra = Dijkstra()
-#     # Graph created
-#     dijkstra.createGraph(vertexes, edges)
-#     # Getting all vertexes
-#     dijkstraVertexes = dijkstra.getVertexes()
-#     # Computing min distance for each vertex in graph
-#     for vertexToCompute in dijkstraVertexes:
-#         dijkstra.computePath(vertexToCompute.id)
-#         print('Printing min distance from vertex:' + str(vertexToCompute.name))
-#         # Print minDitance from current vertex to each other
-#         for vertex in dijkstraVertexes:
-#             print('Min distance to:' + str(vertex.name) + ' is: ' + str(vertex.minDistance))
-#         # Reset Dijkstra between counting
-#         dijkstra.resetDijkstra()
-#     # Distance with path
-#     for vertexToCompute in dijkstraVertexes:
-#         dijkstra.computePath(vertexToCompute.id)
-#         print('Printing min distance from vertex:' + str(vertexToCompute.name))
-#         # Print minDitance and path from current vertex to each other
-#         for vertex in dijkstraVertexes:
-#             print('Min distance to:' + str(vertex.name) + ' is: ' + str(vertex.minDistance))
-#             print('Path is:', end=" ")
-#             # Get shortest path to target vertex
-#             path = dijkstra.getShortestPathTo(vertex.id)
-#             for vertexInPath in path:
-#                 print(str(vertexInPath.name), end=" ")
-#                 print()
-#             # Reset Dijkstra between counting
-#             dijkstra.resetDijkstra()
+def runTests():
+    pass
+
+
+runTests()
